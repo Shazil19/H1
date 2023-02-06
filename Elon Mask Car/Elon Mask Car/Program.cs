089dd5e410d8c29701a37d1494361778d123fdbd
Shazil19 <Shazilshahid04@gmail.com>
2023-02-06 18:21:33 +0100

    06-02-2023
    elon mask car
    superhero
    
    classes, method & objects

---
+++ Elon Mask Car/Elon Mask Car/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Elon_Mask_car
+{
+    public class Car
+    {
+        private string _color;
+        private int _distanceDriven;
+        private int _batteryPercentage;
+
+        // Property to get and set the color of the car
+        public string Color
+        {
+            get { return _color; }
+            private set { _color = value; }
+        }
+
+        // Property to get and set the distance driven by the car
+        public int DistanceDriven
+        {
+            get { return _distanceDriven; }
+            private set { _distanceDriven = value; }
+        }
+
+        // Property to get and set the battery percentage of the car
+        public int BatteryPercentage
+        {
+            get { return _batteryPercentage; }
+            private set { _batteryPercentage = value; }
+        }
+
+        // Constructor to initialize the car object
+        public Car(string color)
+        {
+            _color = color;
+            _distanceDriven = 0;
+            _batteryPercentage = 100;
+        }
+
+        // Method to drive the car
+        public void Drive()
+        {
+            if (_batteryPercentage == 0)
+            {
+                Console.WriteLine("The car cannot drive as the battery is empty.");
+            }
+            else
+            {
+                _distanceDriven += 20;
+                _batteryPercentage -= 1;
+                Console.WriteLine("The car has driven 20 meters and the battery percentage is now " + _batteryPercentage + "%");
+            }
+        }
+
+        // Method to charge the car
+        public void Charge()
+        {
+            _distanceDriven = 0;
+            _batteryPercentage = 100;
+            Console.WriteLine("The car has been charged and the battery percentage is now " + _batteryPercentage + "%");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating two car objects
+            Car car1 = new Car("Red");
+            Car car2 = new Car("Yellow");
+
+            // Looping through the cars until the battery is empty
+            while (car1.BatteryPercentage != 0 && car2.BatteryPercentage != 0)
+            {
+                Console.WriteLine("Choose a car (1 or 2) to drive:");
+                int carChoice = int.Parse(Console.ReadLine());
+
+                if (carChoice == 1)
+                {
+                    car1.Drive();
+                }
+                else if (carChoice == 2)
+                {
+                    car2.Drive();
+                }
+            }
+
+            Console.WriteLine("Both cars cannot drive anymore.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Superhero/Superhero/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Superhero
+    {
+
+        private string _secretidentity;
+
+        //Encapsulation 
+        public string SecretIdentity
+        {
+            get { return _secretidentity; }
+            private set { _secretidentity = value; }
+        }
+
+        private string _powerandabilities;
+
+        //Encapsulation 
+        public string PowersAndAbilities
+        {
+            get { return _powerandabilities; }
+            private set { _powerandabilities = value; }
+        }
+
+        private string _maskandcostume;
+
+        //Encapsulation 
+        public string MaskAndCostume
+        {
+            get { return _maskandcostume; }
+            private set { _maskandcostume = value; }
+        }
+
+        private string _fightaganistcrime;
+
+        //Encapsulation 
+        public string FightAgainstCrime
+        {
+            get { return _fightaganistcrime; }
+            private set { _fightaganistcrime = value; }
+        }
+
+        private string _moralcode;
+
+        //Encapsulation 
+        public string MoralCode
+        {
+            get { return _moralcode; }
+            private set { _moralcode = value; }
+        }
+
+        private string _archenemy;
+
+        //Encapsulation 
+        public string Archenemy
+        {
+            get { return _archenemy; }
+            private set { _archenemy = value; }
+        }
+
+        private string _innerfear;
+
+        //Encapsulation 
+        public string InnerFear
+        {
+            get { return _innerfear; }
+            private set { _innerfear = value; }
+        }
+    }
+}
